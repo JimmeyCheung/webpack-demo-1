61872187886c9435eb4625c01e813cf01e4d93b4
jimmeyCheung <100178981@qq.com>
2020-06-04 22:54:26 +0800

    update

---
+++ deploy_gitee.sh
@@ -0,0 +1,9 @@
+yarn build && 
+git checkout gh-pages &&
+rm -rf *.html *.js *.css *.png &&
+mv dist/* ./&&
+rm -rf dist
+git add . &&
+git commit -m'update'
+git push gitee gh-pages:master &&
+git checkout -
